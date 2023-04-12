7af4636c06144bdcecb3a79c5dd632953f0a6b86
Zuhaib Amjad <umar0423@gmail.com>
2023-04-12 11:02:34 +0500

    Built server with vanilla nodejs

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+const logEvents = require('./logEvents.js');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter {}
+// initialize object
+const myEmitter = new Emitter();
+
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+});
+
+server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+// myEmitter.on('log', (msg) => logEvents(msg));
+// myEmitter.emit('log', 'log event emitted!');
