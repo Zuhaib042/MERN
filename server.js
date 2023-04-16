10821406c70afb435bebe985a5ff6d965e29b03f
Zuhaib Amjad <umar0423@gmail.com>
2023-04-16 13:29:11 +0500

    Add simple routes in server.js file

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-
-const logEvents = require('./logEvents.js');
-const EventEmitter = require('events');
-class Emitter extends EventEmitter {}
-// initialize object
-const myEmitter = new Emitter();
 
 const PORT = process.env.PORT || 3500;
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+app.get('^/$|index(.html)?', (req, res) => {
+  // res.sendFile('./views/index.html', { root: __dirname });
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get('/new-page(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+app.get('/*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
-// myEmitter.on('log', (msg) => logEvents(msg));
-// myEmitter.emit('log', 'log event emitted!');
+app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
